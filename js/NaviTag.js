3dd7d0ecb5850f32d1609c7e905bd3467efdbd74
Dennis Sand <97163222+dennissand@users.noreply.github.com>
2022-03-25 15:51:10 +0100

    Stand heute

---
+++ js/NaviTag.js
@@ -0,0 +1,12 @@
+class NaviTag extends React.Component {
+    render()  {
+        return (<nav>
+            <button><span
+                className="material-icons">sort</span></button>
+            <button><span
+                className="material-icons">settings</span></button>
+            <button><span
+                className="material-icons">edit</span></button>
+            <hr/>
+        </nav>)}
+        }
